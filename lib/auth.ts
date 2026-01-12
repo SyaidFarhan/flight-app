e0218842a3046380b16be08447ede4193846d1d9
syaidt <syaidtengku@gmail.com>
2026-01-12 13:54:53 +0700

    feat : prisma & lucia

---
+++ lib/auth.ts
@@ -0,0 +1,61 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { PrismaClient, RoleUser } from "@prisma/client";
+import { Lucia } from "lucia";
+import { cookies } from "next/headers";
+import { cache } from "react";
+
+const prisma = new PrismaClient();
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        expires: false,
+        attributes: {
+            secure: process.env.NODE_ENV === "production"
+        }
+    },
+    getUserAttributes: (attributes) => {
+        return {
+            name: attributes.name,
+            email: attributes.email,
+            role: attributes.role,
+            password: attributes.password,
+        };
+    }
+});
+
+export const getUser = cache(async () => {
+	const sessionId = (await cookies()).get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) return null;
+	const { user, session } = await lucia.validateSession(sessionId);
+	try {
+		if (session && session.fresh) {
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+		if (!session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+	} catch {
+		// Next.js throws error when attempting to set cookies when rendering page
+	}
+	return user;
+});
+
+
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: {
+      name: string | null;
+      email: string;
+      role: RoleUser;
+      password: string;
+    };
+  }
+}
+
+
