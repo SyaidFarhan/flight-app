18fbdafe1c3b3dbc110fa6abb5e61f76ec8dfc5a
syaidt <syaidtengku@gmail.com>
2026-01-17 13:45:46 +0700

    fix: chang eindex

--- app/dashboard/signin/form/index.tsx
+++ app/dashboard/signin/form/index.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ActionResult, handleSignIn } from "./actions";
 import { FC, useActionState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
 
 
 
@@ -13,6 +14,14 @@ interface SignPageProps {
 const initialFormState : ActionResult = {
     errorTitle : null,
     errorMessage : []
+}
+
+const SubmitPendingButton = () => {
+    const {pending} = useFormStatus();
+    {
+        
+        return<Button type='submit' disabled={pending}  className='w-full py-2 px-4 rounded transition-colors ${pending? gray-500 : black-500}'> {pending ? 'Loading...' : 'Sign In'}</Button>}
+    
 }
 
 
@@ -36,11 +45,19 @@ const FormSignIn: FC<SignPageProps> = ({  }) => {
                 </h2>
             <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-md'>
                 <form action={formAction}>
+                      {state.errorTitle && <div className="pb-1">
+                                            <div className="mb-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                                <span className="block sm:inline">{state.errorTitle}</span>
+                                                <ul className='list-disc list-inside text-sm text-gray-500'>
+                                                    {state.errorMessage && state.errorMessage.map((message, index) => (
+                                                        <li key={index}>{message}</li>
+                                                    ))}
+                                                     </ul>
+                                    </div> 
+                                        </div>}
                     <Input type='email' name='email' placeholder='Email' className='mb-4'/>
                     <Input type='password' name="password" placeholder='Password' className='mb-4'/>
-                    <Button type='submit' className='w-full bg-black text-white py-2 px-4 rounded hover:bg-gray-400 transition-colors' >
-                        Sign In
-                    </Button>
+                    <SubmitPendingButton/>
                 </form>
 
             </div>
